bda2b7fd38679ca0f5444b210d998dc22fc5f570
Luki9 <46124785+Luki9@users.noreply.github.com>
2019-01-08 13:12:37 +0100

    Add files via upload

---
+++ uczesieprogramowania.java
@@ -0,0 +1,12 @@
+public class uczesieprogramowania {
+
+    public static void main(String[] args) {
+
+        System.out.println("ucze");
+        System.out.println("sie");
+        System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
+
+    }
+}
